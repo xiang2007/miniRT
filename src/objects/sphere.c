4292bfeb44dbea1f3167bd3d8f8a4cd5cfead204
shou xiang <weeshouxiang@gmail.com>
2026-04-02 18:01:18 +0800

    created intersection folder

--- src/objects/sphere.c
+++ src/objects/sphere.c
@@ -6,7 +6,7 @@
 /*   By: wshou-xi <wshou-xi@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 14:33:33 by wshou-xi          #+#    #+#             */
-/*   Updated: 2026/03/24 15:24:36 by wshou-xi         ###   ########.fr       */
+/*   Updated: 2026/04/02 16:27:35 by wshou-xi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,6 @@ t_sphere	sphere(t_point3 center, double radius, t_color col)
 	sphere.radius = radius;
 	sphere.color = col.color;
 	return (sphere);
-}
-
-double	hit_sphere(t_sphere sphere, t_ray r)
-{
-	t_vec3	ori_center;
-	t_vec3	center;
-	double	radius;
-	double	a;
-	double	b;
-	double	c;
-	double	d;
-
-	center = sphere.point;
-	radius = sphere.radius;
-	ori_center = sub_vec(center, r.point);
-	a = len_sq(r.vec);
-	b = dot_vec(r.vec, ori_center);
-	c = len_sq(ori_center) - sq(radius);
-	d = sq(b) - a * c;
-	if (d < 0)
-		return (-1);
-	else
-		return ((b - sqrt(d)) / a);
 }
 
 bool front_face(t_cylinder c, t_ray ray, double t)
